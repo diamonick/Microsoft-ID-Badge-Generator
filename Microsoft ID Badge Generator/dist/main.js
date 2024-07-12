c107f6ece990b8754ba702b2e4bfc57e06945b48
DiamonickKCS <46506748+diamonick@users.noreply.github.com>
2024-07-12 18:59:11 -0500

    Add files to repository

---
+++ Microsoft ID Badge Generator/dist/main.js
@@ -0,0 +1,175 @@
+class Swatch
+{
+    selector = "";
+    element;
+    checkmarkIcon;
+    isChecked = false;
+
+    // Swatch Constructor
+    constructor(selector)
+    {
+        this.selector = selector;
+        this.element = document.querySelector(selector);
+        this.checkmarkIcon = document.querySelector(`${selector} .Checkmark-Icon`);
+        this.isChecked = false;
+    }
+
+    checkSwatch()
+    {
+        // Return early if this swatch is already checked.
+        if (this.isChecked)
+            return;
+        
+        this.isChecked = true;
+        this.checkmarkIcon.style.visibility = "visible";
+        setIDBadgeColor(getStyle(this.element, "backgroundColor"));
+
+        for (let swatch of swatches)
+        {
+            // Skip this iteration if the swatch's element matches this element.
+            if (swatch.element == this.element)
+                continue;
+            swatch.uncheckSwatch();
+        }
+    }
+    
+    uncheckSwatch()
+    {
+        this.isChecked = false;
+        this.checkmarkIcon.style.visibility = "hidden";
+    }
+}
+
+//#region Variables
+let employeeName = "";
+let employeeColor = "#ffffff";
+const defaultProfilePicPath = "../Assets/Default Profile Pic.png";
+
+const blueSwatch = new Swatch(".Blue-Swatch");
+const yellowSwatch = new Swatch(".Yellow-Swatch");
+const greenSwatch = new Swatch(".Green-Swatch");
+const redSwatch = new Swatch(".Red-Swatch");
+
+const nameField = document.querySelector(".Name-Field");
+const swatches = [blueSwatch, yellowSwatch, greenSwatch, redSwatch];
+
+const uploadInput = document.querySelector("#upload-input");
+const uploadImageButton = document.querySelector(".Upload-Image-Button");
+const imageIcon = document.querySelector(".Image-Icon");
+const imageFilename = document.querySelector(".Image-File");
+const filename = document.querySelector(".Filename");
+const closeButton = document.querySelector(".Close-Button");
+
+const IDBadgeCover = document.querySelector(".ID-Badge-Cover");
+const employeeNameTitle = document.querySelector(".Employee-Name");
+const employeeProfilePic = document.querySelector(".Employee-Image");
+//#endregion
+
+//#region Main
+function Main()
+{
+    parseNameField();
+    nameField.addEventListener("keyup", parseNameField);
+
+    swatches.forEach(swatch =>{
+        swatch.element.addEventListener("click", function () {swatch.checkSwatch();});
+    });
+
+    // Set the blue swatch as the default swatch preference.
+    blueSwatch.checkSwatch();
+
+    uploadImageButton.addEventListener("click", uploadImage);
+    uploadInput.addEventListener('change', displayEmployeeImage, false);
+    closeButton.addEventListener("click", removeImage);
+}
+//#endregion
+
+function parseNameField()
+{
+    employeeNameTitle.innerHTML = nameField.value;
+}
+
+function uploadImage()
+{
+    uploadInput.click();
+}
+
+/**
+ * Display the user's image on the ID Badge card.
+ */
+function displayEmployeeImage(event)
+{
+    const files = event.target.files;
+    var imageFile = files[0];
+
+    filename.innerHTML = imageFile.name;
+    employeeProfilePic.src = URL.createObjectURL(imageFile);
+
+    // Hide the Upload Image button and display the image's filename.
+    hideElement(uploadImageButton);
+    showElement(imageFilename);
+
+    imageIcon.style.color = "#737373";
+    imageFilename.style.opacity = 0.5;
+    togglePointerEvents(imageFilename, false);
+
+    invoke(enableImageFilename, 0.25);
+}
+
+function setIDBadgeColor(color)
+{
+    var IDBadgeCoverColor = getStyle(IDBadgeCover, "backgroundColor");
+    if (IDBadgeCoverColor == color)
+        return;
+    
+    IDBadgeCover.style.backgroundColor = color;
+}
+
+function enableImageFilename()
+{
+    togglePointerEvents(imageFilename, true);
+    imageFilename.style.opacity = 1;
+}
+
+function removeImage()
+{
+    filename.innerHTML = "";
+    employeeProfilePic.src = defaultProfilePicPath;
+    uploadInput.value = "";
+    
+    // Show the Upload Image button and hide the image's filename.
+    hideElement(imageFilename);
+    showElement(uploadImageButton);
+}
+
+function showElement(element)
+{
+    element.style.display = "flex";
+}
+
+function hideElement(element)
+{
+    element.style.display = "none";
+}
+
+function togglePointerEvents(element, condition)
+{
+    element.style.pointerEvents = condition == true ? "auto" : "none";
+}
+
+function getStyle(element, styleObj)
+{
+    return getComputedStyle(element)[styleObj];
+}
+
+function invoke(func, seconds)
+{
+    let invokeMethod = setTimeout(func, seconds * 1000);
+}
+
+function invokeRepeating(func, seconds)
+{
+    let invokeMethod = setInterval(func, seconds * 1000);
+}
+
+Main();
---
+++ Microsoft ID Badge Generator/tailwind.config.js
@@ -0,0 +1,105 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      
+      screens:
+      {
+        'mobile-sm': '320px',
+        // => @media (min-width: 320px) { ... }
+        
+        'mobile-md': '375px',
+        // => @media (min-width: 375px) { ... }
+
+        'mobile-lg': '425px',
+        // => @media (min-width: 425px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        
+        'md-lg': '896px',
+        // => @media (min-width: 896px) { ... }
+  
+        'lg': '1080px',
+        // => @media (min-width: 1080px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily:
+      {
+        'Inter-Light': ['Inter-Light'],
+        'Inter-Regular': ['Inter-Regular'],
+        'Inter-Medium': ['Inter-Medium'],
+        'Inter-SemiBold': ['Inter-SemiBold'],
+        'Inter-Bold': ['Inter-Bold'],
+        'Inter-ExtraBold': ['Inter-ExtraBold'],
+        'Inter-Black': ['Inter-Black']
+      },
+      fontSize:
+      {
+        '24': '24px',
+        '32': '32px',
+        '48': '48px',
+        '72': '72px'
+      },
+      size:
+      {
+        'swatch': '5.75em'
+      },
+      width:
+      {
+        'col-1': '8.33%',
+        'col-2': '16.66%',
+        'col-3': '25%',
+        'col-4': '33.33%',
+        'col-5': '41.66%',
+        'col-6': '50%',
+        'col-7': '58.33%',
+        'col-8': '66.66%',
+        'col-9': '75%',
+        'col-10': '83.33%',
+        'col-11': '91.66%',
+        'col-12': '100%'
+      },
+      height:
+      {
+        'card-height-sm': '91.66vh',
+        'card-height-sm-1/2': '45.83vh',
+        'card-height': '960px',
+        'card-height-1/2': '540px'
+      },
+      colors:
+      {
+        'microsoft-blue': '#00A3EE',
+        'microsoft-yellow': '#FFB900',
+        'microsoft-green': '#7FBA00',
+        'microsoft-red': '#F25022',
+      },
+      borderRadius:
+      {
+        'default': '1em',
+        '3/4': '0.75em',
+        'swatch': '1em',
+        '48': '48px',
+        'circular': '50%'
+      },
+      boxShadow:
+      {
+        'card-shadow': '0px 8px 16px 0px rgba(0, 0, 0, 0.25)'
+      }
+    },
+  },
+  plugins: [
+    // ... other plugins
+    require('@lostisworld/tailwind-mask'),
+  ],
+}
+
